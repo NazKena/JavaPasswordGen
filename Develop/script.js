695f966c8016ecd773a2df49c5302b43b1e2bc8b
Nuseyba <n.kenadiid@gmail.com>
2022-10-20 21:10:26 +0100

    move index.html

--- Develop/script.js
+++ Develop/script.js
@@ -2,17 +2,17 @@
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword () {
-  const charachterAmountCL = ["a","b","c","d","e","f","g","h","i","j,","k","l","m","n", "o","p","q","r,","s","t","u,","v","w","x","y","z"];
-  const charachterAmountLC = ["A","B","C","D","E","F","G","H","I","J","K","L","M","M","N","O,","P","Q","R","S","T","U","V","W","X","Y","Z"];
-  const charachterAmountNumbers = ["0,1,2,3,4,5,6,7,8,9"];
-  const characterAmountSymbols= ["!","@","£","$","%","%","_","-","<",">",];
+  const charactersAmountCL = ["a","b","c","d","e","f","g","h","i","j,","k","l","m","n", "o","p","q","r,","s","t","u,","v","w","x","y","z"];
+  const charactersAmountLC = ["A","B","C","D","E","F","G","H","I","J","K","L","M","M","N","O,","P","Q","R","S","T","U","V","W","X","Y","Z"];
+  const charactersAmountNumbers = ["0,1,2,3,4,5,6,7,8,9"];
+  const charactersAmountSymbols= ["!","@","£","$","%","%","_","-","<",">",];
 
-  const allDigits = [...charachterAmountCL,...charachterAmountLC,...charachterAmountNumbers,...characterAmountSymbols]
+  const allDigits = [...charactersAmountCL,...charactersAmountLC,...charactersAmountNumbers,...charactersAmountSymbols]
   console.log ("allDigits")
 }
 
 
-let passwordlength = prompt ("How many charachters between 8 and 124 do you want in your password?");{
+let passwordlength = prompt ("How many characters between 8 and 124 do you want in your password?");{
   if (isNaN(passwordlength))
   alert ("no value. please enter a number.");
   if (passwordlength <8 || passwordlength >128)
