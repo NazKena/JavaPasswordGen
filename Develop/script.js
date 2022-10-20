eeaff333a41763116d186db18e4a9d8848495bd9
Nuseyba <n.kenadiid@gmail.com>
2022-10-20 14:27:07 +0100

    copied my function as my previous wasnt working

---
+++ Develop/script.js
@@ -0,0 +1,21 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+function generatePassword () {
+  const charachterAmountCL = ["a","b","c","d","e","f","g","h","i","j,","k","l","m","n", "o","p","q","r,","s","t","u,","v","w","x","y","z"];
+  const charachterAmountLC = ["A","B","C","D","E","F","G","H","I","J","K","L","M","M","N","O,","P","Q","R","S","T","U","V","W","X","Y","Z"];
+  const charachterAmountNumbers = ["0,1,2,3,4,5,6,7,8,9"];
+  const characterAmountSymbols= ["!","@","£","$","%","%","_","-","<",">",];
+
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
